63c2a2d15486b895071225a478e1d02c79ff4d27
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-11-09 21:57:38 -0300

    Algumas operacoes em python

--- src/CalculadoraSimples.py
+++ src/CalculadoraSimples.py
@@ -2,7 +2,7 @@
 
 valor01 = float(input("Qual o primeiro valor?"))
 valor02 = float(input("Qual é o segundo valor?"))
-operacao =  int(input("Selecione + - adição, - - subtração, * - multiplicação, / - divisão"))
+operacao =  input("Selecione + - adição, - - subtração, * - multiplicação, / - divisão")
 
 match operacao:
     case "+":
@@ -13,3 +13,18 @@ match operacao:
         print(valor01*valor02)
     case "/":
         print(valor01/valor02)
+
+if operacao == "+":
+    resultado = valor02+valor01
+    print(resultado)
+elif operacao == "-":
+    resultado = valor01-valor02
+    print(resultado)
+elif operacao == "*":
+    resultado = valor01*valor02
+    print(resultado)
+elif operacao == "/":
+    resultado = valor01/valor02
+    print(resultado)
+else:
+    print("Valor inválido");
---
+++ src/Capitulo04/operacoes.py
@@ -0,0 +1,52 @@
+#soma
+print(4+4)
+
+#sub
+print(4-4)
+
+#multiplicacao
+print(2*2)
+
+#potencia
+print(2**2)
+
+#modulo
+print(10%3)
+
+#funcao para saber o tipo de dado
+a = "Saskjas"
+print(type(5.0))
+print(type(a))
+
+resul = 4.5 + 4
+
+print(resul)
+
+res = 4/2
+
+
+#deixa o resultado float
+print(res)
+
+#forca o resultado para inteiro
+res2 = 4//2
+
+#trouxe o res para float
+
+
+print(res2)
+
+#converter para a base hexa
+print(hex(394))
+
+#converter para binario
+print(bin(654))
+
+#retorna o valor absoluto
+print(abs(-1))
+
+#retorna o arredondado
+print(round(3.333))
+
+#potencia
+print(pow(4,2))
