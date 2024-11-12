c05ba0b1c92cc4f52cec62944b213aebd4c4a114
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-11-12 17:02:55 -0300

    Organizando arquivos em pastas

--- src/Capitulo04/listas.py
+++ src/Capitulo04/listas.py
@@ -37,3 +37,42 @@ print(a)
 listas_totais = listas + b
 
 print(listas_totais)
+
+#FUNÇÕES BUILTIN-IN PARA TRABALHAR COM LISTAS
+
+lista_numeros = [10, 5, 4 ,7]
+
+#tamanho da lista
+len(lista_numeros)
+
+#Pegar o maior numero da lista
+max(lista_numeros)
+
+#Pegar o menor número da lista
+min(lista_numeros)
+
+lista_nomes = ['Aaron', 'Sabrina', 'duda']
+
+#adicionar mais um elemento na lista, como o append se pode adicionar uma string mais de uma vez e precisa tomar cuidado justamente com isso
+lista_nomes.append("Issllany")
+lista_nomes.append("Issllany")
+print(lista_nomes.count('Issllany'))
+print(lista_nomes)
+
+a = []
+
+print(a)
+print(type(a))
+
+a.append(3)
+
+print(a)
+
+old_list = [1, 3, 7, 8]
+new_list = []
+
+#o item vai percorrer tds os elementos da lista old_list e adicionar na new_list
+for item in old_list:
+    new_list.append(item)
+
+    print(new_list)
