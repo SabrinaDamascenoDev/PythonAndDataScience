d9d4d38f9e9507a9c4b01da821114b156c02b267
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-11-12 21:41:04 -0300

    Condicionais em python

---
+++ src/Capitulo05/condicionais.py
@@ -0,0 +1,21 @@
+#ifs
+
+if 5 > 2:
+    print("Its true, bebe")
+else:
+    print("Its not, bebe")
+
+dia = "Terca"
+
+if dia == "segunda":
+    print("Hoje é segunda")
+else:
+    print("hj é terça")
+
+
+if dia == "Segunda":
+    print("Hoje é segunda")
+elif dia == "Terca":
+    print("Hoje é terça")
+else:
+    print("Não é nenhum dos dois dias")
