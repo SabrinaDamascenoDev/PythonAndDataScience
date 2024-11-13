330c5e0e3e0caa751d7b5627d4e21fc3afabb028
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-11-13 19:38:22 -0300

    trabalhando com loops

--- src/Capitulo05/loopWhile.py
+++ src/Capitulo05/loopWhile.py
@@ -1,4 +1,5 @@
 
+
 valor = 0
 #se não tiver o incremento vai ficar em loop infinito
 while valor < 10:
@@ -29,3 +30,15 @@ for letra in "Python é zzz incrivel":
     print(letra)
 
 #Loop while and for juntos
+lista = []
+
+#minha forma com for + for
+for i in range(1, 31):
+    divisiveis = 0;
+    for j in range(1, 31):
+        if i%j == 0:
+            divisiveis = divisiveis+1
+    if divisiveis == 2:
+        lista.append(i)
+
+print(lista)
