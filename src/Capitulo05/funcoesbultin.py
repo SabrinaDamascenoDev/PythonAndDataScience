c6c1809223227176c7ecf33822b33fee5af79aed
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-11-19 13:16:35 -0300

    Funcoes

---
+++ src/Capitulo05/funcoesbultin.py
@@ -0,0 +1,30 @@
+#Funcoes Built-in
+#Valor absoluto
+print(abs(-56))
+#boolean
+bool()
+#Converte para inteiro
+int(4.3)
+
+#Converte um valor para float
+float(5)
+#Pedir para o cliente colocar, só recebe string
+input()
+
+#Converter para int o valor de String que o input coloca auto
+int(input())
+
+#Transformar um num em uma string
+str(45)
+
+#conta o tamanho da lista
+len([32, 45, 67])
+
+array = [1, 2, 3]
+
+#achar valor maximo e minimo
+print(max(array))
+print(min(array))
+
+#Somar numeros de uma lista
+print(sum(array))
