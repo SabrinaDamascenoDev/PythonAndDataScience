2421f8fdf8ef27541bec738755f369c5d8d27c57
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-11-20 09:28:36 -0300

    Juntando funcoes

---
+++ src/Capitulo05/funcoesJuntas.py
@@ -0,0 +1,11 @@
+import math
+
+def numPrimo(num):
+    if(num%2 == 0) and num > 2:
+        return "Esse número não é primo"
+    for i in range(3, int(math.sqrt(num)) + 1, 2):
+        if (num%i) == 0:
+            return "Esse número não é primo"
+    return "É primo"
+
+print(numPrimo(5))
