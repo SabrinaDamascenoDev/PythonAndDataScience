f00319f5d13a1cf6e28064cccd53876a9d7221a7
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-11-21 17:59:24 -0300

    Alguns exercícios sobre func

---
+++ src/Capitulo05/Exercicios/Funcoes/maiusculas.py
@@ -0,0 +1,3 @@
+def maiusculas(text):
+    return text.upper()
+print(maiusculas('sabrina'))
---
+++ src/Capitulo05/Exercicios/Funcoes/pares.py
@@ -0,0 +1,6 @@
+def pases():
+    numerosPares = []
+    for i in range(2,- 21, 2):
+        numerosPares.append(i)
+    return numerosPares
+print(pases())
